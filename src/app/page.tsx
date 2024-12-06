42323c9948b240cd874d39cbc117f1730c9cc002
Kyle Oliva <kecoliva@gmail.com>
2024-12-05 22:55:34 -0500

    add kube, docker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,34 @@
-
 export default function Home() {
   return (
     <div className="container">
-        <h1>IN-CLASS ACTIVITY :- 3</h1>
-        <h2>Group: 3</h2>
-        <table>
-            <thead>
-                <tr>
-                    <th>Student Name</th>
-                    <th>Student ID</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Aashish Tuladhar</td>
-                    <td>C0909094</td>
-                </tr>
-                <tr>
-                    <td>Biraj Raya</td>
-                    <td>C0906769</td>
-                </tr>
-                <tr>
-                    <td>Kyle Elijah Oliva</td>
-                    <td>C0919468</td>
-                </tr>
-                <tr>
-                    <td>Sudeep Banjade</td>
-                    <td>C0911574</td>
-                </tr>
-            </tbody>
-        </table>
+      <h1>IN-CLASS ACTIVITY :- 3</h1>
+      <h2>Group: 3</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Student Name</th>
+            <th>Student ID</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Aashish Tuladhar</td>
+            <td>C0909094</td>
+          </tr>
+          <tr>
+            <td>Biraj Raya</td>
+            <td>C0906769</td>
+          </tr>
+          <tr>
+            <td>Kyle Elijah Oliva</td>
+            <td>C0919468</td>
+          </tr>
+          <tr>
+            <td>Sudeep Banjade</td>
+            <td>C0911574</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
